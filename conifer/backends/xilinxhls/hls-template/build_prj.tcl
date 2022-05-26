1d6ea609c888b771e796470d8e398ae2df59eebd
David Reikher <david.reikher@gmail.com>
2022-05-26 22:39:06 +0200

    Add uncertainty and reset port off

--- conifer/backends/xilinxhls/hls-template/build_prj.tcl
+++ conifer/backends/xilinxhls/hls-template/build_prj.tcl
@@ -40,6 +40,8 @@ if {$opt(reset)} {
 open_solution -reset "solution1"
 set_part {xc7vx690tffg1927-2}
 create_clock -period 5 -name default
+set_clock_uncertainty 3
+config_rtl -reset none
 
 if {$opt(csim)} {
     csim_design
